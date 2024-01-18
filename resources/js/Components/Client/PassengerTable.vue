5683cb46048a5dfdfe838094d2c9ca0679fb7875
alexandruana <anaalexandru1996@gmail.com>
2024-01-18 12:25:50 +0100

    Renamed PassengerFormModal.vue

--- resources/js/Components/Client/PassengerTable.vue
+++ resources/js/Components/Client/PassengerTable.vue
@@ -2,7 +2,7 @@
 import { defineProps, ref } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import PassengerProfileModal from '@/Components/Client/PassengerProfileModal.vue'
-import PassengerFormModal from '@/Components/Client/PassengerFormModal.vue'
+import AddPassengerModal from '@/Components/Client/AddPassengerModal.vue'
 
 // Define props
 const props = defineProps({
@@ -44,7 +44,7 @@ const deletePassenger = (id) => {
                     <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                 </svg>
             </button>
-            <PassengerFormModal v-if="showPassengerForm" @close-modal="toggleModal"/>
+            <AddPassengerModal v-if="showPassengerForm" @close-modal="toggleModal"/>
         </div>
         <table class="table-auto w-full">
             <thead>
